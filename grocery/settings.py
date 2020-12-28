3f2e821dbde93507d719ad79c92be6b1607f0b61
SAAD <57146004+SaadFurqhan@users.noreply.github.com>
2020-12-28 16:50:34 +0530

    Update settings.py

--- grocery/settings.py
+++ grocery/settings.py
@@ -130,6 +130,6 @@ MEDIA_URL = "/image/download/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT =os.path.join(BASE_DIR,'uploads/products')
