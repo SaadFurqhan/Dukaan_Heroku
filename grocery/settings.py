634c589bc9d762b689f7acca2fe1b5c8a47e4952
SAAD <57146004+SaadFurqhan@users.noreply.github.com>
2020-12-28 23:57:12 +0530

    Update settings.py

--- grocery/settings.py
+++ grocery/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 MEDIA_URL = "/uploads/products/"
@@ -132,4 +132,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = BASE_DIR
+# os.path.join(BASE_DIR, 'media')
